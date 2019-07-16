12f4ca868a52635ef919b383c3543f24fd9a1b2e
josh182014 <josh182014@gmail.com>
2019-07-16 11:11:01 -0600

    set up basic react app with navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Navigation.scss";
+
+const Nav = () => {
+    return (
+        <>
+            <header className="header">
+                <h3>joshtimmons.dev</h3>
+                <div className='nav'>
+                    <div>Home</div>
+                    <div>About</div>
+                    <div>Contact</div>
+                </div>
+        </header>
+        </>
+    );
+}
+ 
+export default Nav;
