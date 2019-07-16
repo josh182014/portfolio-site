9277126483e768c21b397493965e26c353923c00
josh182014 <josh182014@gmail.com>
2019-07-16 16:10:43 -0600

    added images to projects

--- src/components/Projects/dummyProjects.js
+++ src/components/Projects/dummyProjects.js
@@ -1,9 +1,9 @@
 const dummyProjects = [
     {
-        name: 'Test Poject', github: 'github.com', mainLanguage: 'javascript', description: 'This is a test description for my test project. Bla bla bla.'
+        name: 'Test Poject', github: 'github.com', mainLanguage: 'javascript', description: 'This is a test description for my test project. Bla bla bla.', img: 'assets/great-idea.PNG'
     },
     {
-        name: 'Test Project Number 2', github: 'github.com', mainLanguage: 'python', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', viewLive: 'http://netlify.com'
+        name: 'Test Project Number 2', github: 'github.com', mainLanguage: 'python', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', viewLive: 'http://netlify.com', img: '/assets/bleep.png'
     }
 
 ]
