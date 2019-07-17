73866f18d6a338e9a8f3263514d8f6db0ea9fcb5
josh182014 <josh182014@gmail.com>
2019-07-17 15:24:15 -0600

    switched navigation to a hamburger navigation

--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -2,17 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Navigation.scss";
 
+const handleClick = () => {
+    document.querySelector('.hamburger1').classList.toggle("change1");
+    document.querySelector('.hamburger2').classList.toggle("change2");
+    document.querySelector('.hamburger3').classList.toggle("change3");
+    document.querySelector('.new-nav').classList.toggle('hidden-new-nav');
+
+}
+
 const Nav = () => {
     return (
         <div className="navContainer">
             <header className="header">
                 <h3>joshtimmons.dev</h3>
                 <div className='nav'>
+                    {/* <Link className='navLink' to="/">Home</Link>
+                    <Link className='navLink' to="/about">About</Link>
+                    <Link className='navLink' to="/contact">Contact</Link> */}
+                <nav onClick={handleClick} class='hamburger-nav hidden-hamburger-nav'>
+                        <div class='hamburger hamburger1'></div>
+                        <div class='hamburger hamburger2'></div>
+                        <div class='hamburger hamburger3'></div>
+				    </nav>
+                </div>
+        </header>
+            <div onClick={handleClick} class='new-nav hidden-new-nav'>
+                <nav class="new-nav-links">
+                    <h2>Menu</h2>
                     <Link className='navLink' to="/">Home</Link>
                     <Link className='navLink' to="/about">About</Link>
                     <Link className='navLink' to="/contact">Contact</Link>
-                </div>
-        </header>
+                </nav>
+            </div>
         </div>
     );
 }
