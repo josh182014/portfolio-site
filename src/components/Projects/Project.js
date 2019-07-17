9bf150748d2ad0eec1541dd156aee7ac0528c430
josh182014 <josh182014@gmail.com>
2019-07-17 13:40:23 -0600

    converted to using flexbox on home page styles

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -3,12 +3,14 @@ import React from 'react';
 const Project = (props) => {
     return (
         <div key={props.project.name} className='projectContainer'>
-            <h3>Name: {props.project.name}</h3>
-            <p>Description: {props.project.description}</p>
+            <div className='projectTextContainer'>
+                <h3>Name: {props.project.name}</h3>
+                <div className='projectDescription'>Description: {props.project.description}</div>
+                <div className='projectDiv'>Main Language: {props.project.mainLanguage}</div>
+                <a className='projectDiv projectGitLink' href={props.project.github}>View Code</a>
+                {props.project.viewLive ? (<a className='projectDiv projectLiveLink' rel="noopener noreferrer" target='_blank' href={props.project.viewLive}> View Live </a>) : (false)}
+                </div>
             <img src={props.project.img} />
-            <div className='projectDiv'>Main Language: {props.project.mainLanguage}</div>
-            <a className='projectDiv projectGitLink' href={props.project.github}>View Code</a>
-            {props.project.viewLive ? (<a className='projectDiv projectLiveLink' rel="noopener noreferrer" target='_blank' href={props.project.viewLive}> View Live </a>) : (false)}
             <div className='split' ></div>
         </div>
     )
