3a1170edfaa37014447f292d684c0464c372e3dc
josh182014 <josh182014@gmail.com>
2019-07-17 14:11:52 -0600

    fixed styling issues. Now has a max width on containers

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -6,7 +6,7 @@ const Project = (props) => {
             <div className='projectTextContainer'>
                 <h3>Name: {props.project.name}</h3>
                 <div className='projectDescription'>Description: {props.project.description}</div>
-                <div className='projectDiv'>Main Language: {props.project.mainLanguage}</div>
+                {/* <div className='projectDiv'>Main Language: {props.project.mainLanguage}</div> */}
                 <a className='projectDiv projectGitLink' href={props.project.github}>View Code</a>
                 {props.project.viewLive ? (<a className='projectDiv projectLiveLink' rel="noopener noreferrer" target='_blank' href={props.project.viewLive}> View Live </a>) : (false)}
                 </div>
