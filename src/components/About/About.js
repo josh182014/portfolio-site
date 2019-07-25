788f49b1f66f6f6fc2cb60e6977d69dd8e68cab9
josh182014 <josh182014@gmail.com>
2019-07-25 14:51:42 -0600

    changed dark blue colors

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className='about-container'>
             <h1>About</h1>
-            <p>Coming soon...</p>
+            <p>I am an extremely motivated Full Stack Software Developer seeking a career near Austin, TX. I am currently attending Lambda School's Full Stack Web Development course.</p>
         </div>
     );
 }
